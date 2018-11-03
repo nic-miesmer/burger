04046e44acc6ed723a6a705fc90d630ee3b0a661
nmiesmer <nic.miesmer@segmint.com>
2018-11-03 12:31:36 -0400

    added database, and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Regular', false);
